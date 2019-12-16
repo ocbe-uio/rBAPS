002c0db3f99ac834c8714b2c5cc1872d8b8bf095
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-12-16 16:36:51 +0100

    Changed behavior of log(gamma(x))

--- R/calculatePopLogml.R
+++ R/calculatePopLogml.R
@@ -4,16 +4,17 @@
 #' for the mean parameter.
 #' @param points points
 #' @param fii fii
+#' @export
 calculatePopLogml <- function(points, fii) {
     n <- length(points)
     fuzzy_ones <- sum(points)
     fuzzy_zeros <- n - fuzzy_ones
-    val = log(gamma(1)) -
-        log(gamma(1 + n / fii)) +
-        log(gamma(0.5 + fuzzy_ones / fii)) +
-        log(gamma(0.5 + fuzzy_zeros / fii)) -
-        log(gamma(0.5)) -
-        log(gamma(0.5))
+    val <- log_gamma(1) -
+        log_gamma(1 + n / fii) +
+        log_gamma(0.5 + fuzzy_ones / fii) +
+        log_gamma(0.5 + fuzzy_zeros / fii) -
+        log_gamma(0.5) -
+        log_gamma(0.5)
     return(val)
 }
 
