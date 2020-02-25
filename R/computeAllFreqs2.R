70528756fc554001578114ffcf976a98dbb7622a
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-02-25 11:52:41 +0100

    Added computeAllFreqs2 function

---
+++ R/computeAllFreqs2.R
@@ -0,0 +1,23 @@
+#' @title Compute all freqs - version 2
+#' @description Lisää a priori jokaista alleelia joka populaation joka lokukseen
+#' j 1/noalle(j) verran.
+computeAllFreqs2 <- function (noalle, COUNTS = matrix(NA, 0, 0),
+    SUMCOUNTS = sum(COUNTS)) {
+
+    max_noalle <- size(COUNTS, 1)
+    nloci <- size(COUNTS,2)
+    npops <- size(COUNTS,3)
+
+    sumCounts <- SUMCOUNTS + ones(size(SUMCOUNTS))
+    sumCounts <- reshape(t(sumCounts), c(1, nloci, npops))
+    sumCounts <- repmat(sumCounts, c(max_noalle, 1, 1))
+
+    prioriAlleelit <- zeros(max_noalle, nloci)
+    for (j in 1:nloci) {
+         prioriAlleelit[1:noalle[j], j] <- 1 / noalle[j]
+    }
+    prioriAlleelit <- repmat(prioriAlleelit, c(1, 1, npops))
+    counts <- COUNTS + prioriAlleelit
+    allFreqs <- counts / sumCounts
+    return(allFreqs)
+}
