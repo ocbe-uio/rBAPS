aa9f1c4ef65dc0d44e6927126e654cf78cc5c854
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-07-14 13:37:56 +0200

    Translated initPopNames

--- R/greedyMix.R
+++ R/greedyMix.R
@@ -1000,49 +1000,6 @@ greedyMix <- function(
 
 # 	%-----------------------------------------------------------------------------------
 
-
-# 	function popnames = initPopNames(nameFile, indexFile)
-# 	%Palauttaa tyhj�n, mik�li nimitiedosto ja indeksitiedosto
-# 	% eiv�t olleet yht?pitki?
-
-# 	popnames = [];
-# 	indices = load(indexFile);
-
-# 	fid = fopen(nameFile);
-# 	if fid == -1
-# 		%File didn't exist
-# 		msgbox('Loading of the population names was unsuccessful', ...
-# 			'Error', 'error');
-# 		return;
-# 	end;
-# 	line = fgetl(fid);
-# 	counter = 1;
-# 	while (line ~= -1) & ~isempty(line)
-# 		names{counter} = line;
-# 		line = fgetl(fid);
-# 		counter = counter + 1;
-# 	end;
-# 	fclose(fid);
-
-# 	if length(names) ~= length(indices)
-# 		disp('The number of population names must be equal to the number of ');
-# 		disp('entries in the file specifying indices of the first individuals of ');
-# 		disp('each population.');
-# 		return;
-# 	end
-
-# 	popnames = cell(length(names), 2);
-# 	for i = 1:length(names)
-# 		popnames{i,1} = names(i);
-# 		popnames{i,2} = indices(i);
-# 	end
-
-
-# 	%-----------------------------------------------------------------------------------
-
-
-# 	%--------------------------------------------------------------------------
-
 # 	function logml = ...
 # 		initialCounts(partition, data, npops, rows, noalle, adjprior)
 
---
+++ R/initPopNames.R
@@ -0,0 +1,37 @@
+#' @title Initialize Pop Names
+#' @param nameFile nameFile
+#' @param indexFile indexFile
+#' @export
+initPopNames <- function(nameFile, indexFile) {
+	# Palauttaa tyhj�n, mik�li nimitiedosto ja indeksitiedosto
+	#  eiv�t olleet yht?pitki?
+
+	indices <- load(indexFile)
+
+	fid = load(nameFile)
+	if (fid == -1) {
+		# File didn't exist
+		stop('Loading of the population names was unsuccessful')
+	}
+	line <- readLines(fid)[1]
+	counter <- 1
+	names <- vector()
+	while ((line != -1) & (line != "")) {
+		names[counter] <- line
+		line <- readLines(fid)[counter]
+		counter <- counter + 1
+	}
+
+	if (length(names) != length(indices)) {
+		cat('The number of population names must be equal to the number of')
+		cat('entries in the file specifying indices of the first individuals')
+		cat('of each population.')
+	}
+
+	popnames <- cell(length(names), 2)
+	for (i in 1:length(names)) {
+		popnames[i, 1] <- names[i]
+		popnames[i, 2] <- indices[i]
+	}
+	return(popnames)
+}
