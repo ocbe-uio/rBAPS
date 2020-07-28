2f809d01df29b58c9eb781f4881d0f38faae5eed
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-07-28 15:35:17 +0200

    Improved validation

--- R/lueNimi.R
+++ R/lueNimi.R
@@ -4,6 +4,12 @@
 #' @return nimi
 #' @export
 lueNimi <- function(line) {
+	# ==========================================================================
+	# Validation
+	# ==========================================================================
+	if (!grepl(",", line)) {
+		stop("There are no commas in this line")
+	}
 	# Palauttaa line:n alusta sen osan, joka on ennen pilkkua.
 	n <- 1
 	merkki <- substring(line, n, n)
