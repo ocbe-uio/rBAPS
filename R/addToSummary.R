74e11205b72092508a13a18125f1bdf5a9704fcd
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-10-19 15:58:08 +0200

    Fixed syntax

--- R/addToSummary.R
+++ R/addToSummary.R
@@ -1,4 +1,4 @@
-addToSummary <- funciton(logml, partitionSummary, worstIndex) {
+addToSummary <- function(logml, partitionSummary, worstIndex) {
 	# Tiedet��n, ett� annettu logml on isompi kuin huonoin arvo
 	# partitionSummary taulukossa. Jos partitionSummary:ss� ei viel� ole
 	# annettua logml arvoa, niin lis�t��n worstIndex:in kohtaan uusi logml ja
@@ -6,13 +6,13 @@ addToSummary <- funciton(logml, partitionSummary, worstIndex) {
 
 	apu <- find(abs(partitionSummary[, 2] - logml) < 1e-5)
 	if (isempty(apu)) {
-	    # Nyt l�ydetty partitio ei ole viel� kirjattuna summaryyn.
-	    npops <- length(unique(PARTITION))
-	    partitionSummary[worstIndex, 1] <- npops
-	    partitionSummary[worstIndex, 2] <- logml
-	    added <- 1
+		# Nyt l�ydetty partitio ei ole viel� kirjattuna summaryyn.
+		npops <- length(unique(PARTITION))
+		partitionSummary[worstIndex, 1] <- npops
+		partitionSummary[worstIndex, 2] <- logml
+		added <- 1
 	} else {
-	    added <- 0
+		added <- 0
 	}
 	return(list(partitionSummary = partitionSummary, added = added))
 }
