457b352b9c48eb14ba1c347ec9a3337596b8255a
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-10-19 14:14:06 +0200

    Translated addToSummary

--- R/indMix.R
+++ R/indMix.R
@@ -1191,26 +1191,6 @@ indMix <- function(c, npops, dispText) {
 
 # %--------------------------------------------------------------------------
 
-# function [partitionSummary, added] = addToSummary(logml, partitionSummary, worstIndex)
-# % Tiedet��n, ett� annettu logml on isompi kuin huonoin arvo
-# % partitionSummary taulukossa. Jos partitionSummary:ss� ei viel� ole
-# % annettua logml arvoa, niin lis�t��n worstIndex:in kohtaan uusi logml ja
-# % nykyist� partitiota vastaava nclusters:in arvo. Muutoin ei tehd� mit��n.
-
-# apu = find(abs(partitionSummary(:,2)-logml)<1e-5);
-# if isempty(apu)
-#     % Nyt l�ydetty partitio ei ole viel� kirjattuna summaryyn.
-#     global PARTITION;
-#     npops = length(unique(PARTITION));
-#     partitionSummary(worstIndex,1) = npops;
-#     partitionSummary(worstIndex,2) = logml;
-#     added = 1;
-# else
-#     added = 0;
-# end
-
-# %--------------------------------------------------------------------------
-
 # function inds = returnInOrder(inds, pop, globalRows, data, ...
 #     adjprior, priorTerm)
 # % Palauttaa yksil�t j�rjestyksess� siten, ett� ensimm�isen� on
