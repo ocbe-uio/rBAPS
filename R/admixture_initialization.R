f886feb3a8bdc205542364019d32092d4306797f
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-10-19 13:44:18 +0200

    Translated admixture_initialization

---
+++ R/admixture_initialization.R
@@ -0,0 +1,17 @@
+#'@title Seuraavat kolme funktiota liittyvat alkupartition muodostamiseen.
+
+admixture_initialization <- function (data_matrix, nclusters, Z) {
+	size_data <- size(data_matrix)
+	nloci <- size_data[2] - 1
+	n <- max(data_matrix[, end])
+	T <- cluster_own(Z, nclusters)
+	initial_partition <- zeros(size_data[1], 1)
+	for (i in 1:n) {
+		kori <- T[i]
+		here <- find(data_matrix[,end] == i)
+		for (j in 1:length(here)) {
+			initial_partition[here[j], 1] <- kori
+		}
+	}
+	return(initial_partition)
+}
--- R/indMix.R
+++ R/indMix.R
@@ -1157,22 +1157,6 @@ indMix <- function(c, npops, dispText) {
 
 
 # %--------------------------------------------------------------------------
-# %Seuraavat kolme funktiota liittyvat alkupartition muodostamiseen.
-
-# function initial_partition=admixture_initialization(data_matrix,nclusters, Z)
-
-# size_data=size(data_matrix);
-# nloci=size_data(2)-1;
-# n=max(data_matrix(:,end));
-# T=cluster_own(Z,nclusters);
-# initial_partition=zeros(size_data(1),1);
-# for i=1:n
-#     kori=T(i);
-#     here=find(data_matrix(:,end)==i);
-#     for j=1:length(here)
-#         initial_partition(here(j),1)=kori;
-#     end
-# end
 
 # function T = cluster_own(Z,nclust)
 # true=logical(1);
