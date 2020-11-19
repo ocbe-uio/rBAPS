46a9249e5ad7df702c19df9c8f26f6bc65f0adf1
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-11-19 07:53:36 +0100

    Fixed syntax

--- R/admixture_initialization.R
+++ R/admixture_initialization.R
@@ -6,12 +6,12 @@
 admixture_initialization <- function (data_matrix, nclusters, Z) {
 	size_data <- size(data_matrix)
 	nloci <- size_data[2] - 1
-	n <- max(data_matrix[, end])
+	n <- max(data_matrix[, ncol(data_matrix)])
 	T <- cluster_own(Z, nclusters)
 	initial_partition <- zeros(size_data[1], 1)
 	for (i in 1:n) {
 		kori <- T[i]
-		here <- find(data_matrix[,end] == i)
+		here <- find(data_matrix[, ncol(data_matrix)] == i)
 		for (j in 1:length(here)) {
 			initial_partition[here[j], 1] <- kori
 		}
