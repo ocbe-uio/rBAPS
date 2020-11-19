dc9b4f7f9cb44ab171eb3e6d54737eb88e3167ef
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-11-19 08:44:38 +0100

    Translated initializeGammaln

---
+++ R/initializeGammaln.R
@@ -0,0 +1,9 @@
+initializeGammaln <- function(ninds, rowsFromInd, maxAlleles) {
+	#Alustaa GAMMALN muuttujan s.e. GAMMALN(i, j)=gammaln((i - 1) + 1/j)
+	GAMMA_LN <- zeros((1 + ninds) * rowsFromInd, maxAlleles)
+	for (i in 1:(ninds + 1) * rowsFromInd) {
+		for (j in 1:maxAlleles) {
+			GAMMA_LN[i, j] <- log_gamma((i - 1) + 1/j)
+		}
+	}
+}
