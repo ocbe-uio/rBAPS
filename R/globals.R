3b423cdb09c590949922490539e0f2ca828448f8
Waldir Leoncio <waldir.leoncio@gmail.com>
2021-01-15 10:08:15 +0100

    Updated docs

--- R/globals.R
+++ R/globals.R
@@ -3,6 +3,8 @@ SUMCOUNTS <- vector()
 PARTITION <- vector()
 POP_LOGML <- vector()
 LOGDIFF <- vector()
+# If handling globas break, try other ideas from https://stackoverflow.com/a/65252740/1169233
+
 
 #' @import utils
 utils::globalVariables(
