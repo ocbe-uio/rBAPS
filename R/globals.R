44210e97373bd55c20419a7bf5724df8ce4dae97
Waldir Leoncio <waldir.leoncio@gmail.com>
2021-02-01 10:01:25 +0100

    Changed global definitions

--- R/globals.R
+++ R/globals.R
@@ -1,8 +1,8 @@
 COUNTS <- array(0, dim=c(100, 100, 100))
 SUMCOUNTS <- array(0, dim=c(100, 100))
-PARTITION <- vector()
-POP_LOGML <- vector()
-LOGDIFF <- vector()
+PARTITION <- array(1, dim=c(100))
+POP_LOGML <-  array(1, dim=c(100))
+LOGDIFF <-  array(1, dim=c(100, 100))
 # If handling globas break, try other ideas from https://stackoverflow.com/a/65252740/1169233
 
 
