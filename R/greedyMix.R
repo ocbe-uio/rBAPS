c7720f33b4f18ad34483d46f24a9e1dda8bdad2e
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-03-30 08:23:37 +0200

    Fixed output formatting

--- R/greedyMix.R
+++ R/greedyMix.R
@@ -75,12 +75,12 @@ greedyMix <- function(
 			# ASK: remove?
 			# h0 = findobj('Tag','filename1_text');
 			# set(h0,'String',filename); clear h0;
-			cat(
+			message(
 				'When using data which are in BAPS-format,',
 				'you can specify the sampling populations of the',
 				'individuals by giving two additional files:',
 				'one containing the names of the populations,',
-				'the other containing the indices of the first',
+				'the other containing the indices of the first ',
 				'individuals of the populations.'
 			)
 			input_pops <- inputdlg(
