231e0ff501dd195e3aae7ae829ca69ffe0894742
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-03-30 08:23:21 +0200

    Fixed syntax

--- R/testaaOnkoKunnollinenBapsData.R
+++ R/testaaOnkoKunnollinenBapsData.R
@@ -7,13 +7,13 @@ testaaOnkoKunnollinenBapsData <- function(data) {
 	# Tarkastaa onko viimeisess?sarakkeessa kaikki
 	# luvut 1,2,...,n johonkin n:��n asti.
 	# Tarkastaa lis�ksi, ett?on v�hint��n 2 saraketta.
-	if (size[data, 1] < 2) {
+	if (size(data, 1) < 2) {
 		ninds <- 0
 		return(ninds)
 	}
 	lastCol <- data[, ncol(data)]
 	ninds <- max(lastCol)
-	if (t(1:ninds) != unique(lastCol)) {
+	if (any(1:ninds != unique(lastCol))) {
 		ninds <- 0
 		return(ninds)
 	}
