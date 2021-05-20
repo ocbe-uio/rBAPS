c975a7c820fcf5374891403afb7958942cbf3219
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-05-20 09:18:33 +0200

    Removed duplicated function

--- R/cluster_own.R
+++ R/cluster_own.R
@@ -33,19 +33,3 @@ cluster_own <- function(Z, nclust) {
 	}
 	return(T)
 }
-
-clusternum <- function(X, T, k, c) {
-	m <- size(X, 1) + 1
-	while (!isempty(k)) {
-		# Get the children of nodes at this level
-		children <- X[k, 1:2]
-		children <- children
-
-		# Assign this node number to leaf children
-		t <- (children <= m)
-		T[children[t]] <- c
-		# Move to next level
-		k <- children(!t) - m
-	}
-	return(T)
-}
