db760fe899335a3a7bfd8a72e5b37231c1899a7b
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-08-23 14:34:20 +0200

    Minor changes for clean check

--- R/isGlobalEmpty.R
+++ R/isGlobalEmpty.R
@@ -3,6 +3,7 @@
 #' @details For a list of global variables, check the \code{globals.R} file.
 #' @param g the global variable in quesiton.
 #' @return TRUE if the variable still contains its original values, FALSE otherwise.
+#' @importFrom stats sd
 #' @author Waldir Leoncio
 isGlobalEmpty <- function(g) {
 	return(sum(g) == 0 & sd(g) == 0)
