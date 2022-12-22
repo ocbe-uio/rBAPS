6f8e1a51a2dd835d13c3a3f728e6cb74d7c57218
Waldir Leoncio <w.l.netto@medisin.uio.no>
2022-12-22 14:38:47 +0100

    Translated mysize()

---
+++ R/mysize.R
@@ -0,0 +1,17 @@
+mysize <- function(M) {
+  #  MYSIZE Like the built - in size, except it returns n if (M is a vector of length n, and 1 if M is a scalar.) {
+  #  sz <- mysize(M)
+  #  The behavior is best explained by examples
+  #  - M <- rand(1, 1),   mysize[M] <- 1,      size(M) = [1 1]
+  #  - M <- rand(2, 1),   mysize[M] <- 2,      size(M) = [2 1]
+  #  - M <- rand(1, 2),   mysize[M] <- 2,      size(M) = [1 2]
+  #  - M <- rand(2, 2,1), mysize[M] <- [2 2],  size(M) = [2 2]
+  #  - M <- rand(1, 2,1), mysize[M] <- 2,      size(M) = [1 2]
+
+  if (myisvector(M)) {
+    sz <- length(M)
+  } else {
+    sz <- size(M)
+  }
+  return(sz)
+}
