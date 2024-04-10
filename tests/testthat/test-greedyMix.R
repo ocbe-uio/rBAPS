2a815f15cd7235182efd05fe4eaef94e7b805409
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-04-10 16:06:23 +0200

    Added (commented) test for BAPS data (#24)

--- tests/testthat/test-greedyMix.R
+++ tests/testthat/test-greedyMix.R
@@ -46,6 +46,11 @@ raw_bam <- importFile(
   data    = file.path(path_inst, "bam_example.bam"),
   format  = "BAM",
 )
+# TODO: uncomment for testing #24
+# raw_baps <- importFile(
+#   data   = file.path(path_inst, "FASTA_clustering_haploid.fasta"),
+#   format = "FASTA"
+# )
 
 test_that("Files are imported correctly", {
   expect_equal(dim(raw_fasta), c(5, 99))
