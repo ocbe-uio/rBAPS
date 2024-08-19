b7282d67dfee26db7fa73f6c332a5ce7c807b1bc
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-08-19 13:32:05 +0200

    Adapted tests to FASTA on greedyMix() (#24)

--- tests/testthat/test-greedyMix.R
+++ tests/testthat/test-greedyMix.R
@@ -57,13 +57,6 @@ test_that("Files are imported correctly", {
     )
   )
   expect_equal(length(raw_bam[[1]]), 13)
-  expect_error(
-    greedyMix(
-      data   = file.path(path_inst, "FASTA_clustering_haploid.fasta"),
-      format = "FASTA"
-    ),
-    "FASTA format not yet supported on greedyMix"
-  )
 })
 
 test_that("greedyMix() fails successfully", {
@@ -73,9 +66,13 @@ test_that("greedyMix() fails successfully", {
 
 test_that("greedyMix() works when it should", {
   baps_file <- file.path(path_inst, "BAPS_clustering_diploid.txt")
+  fasta_file <- file.path(path_inst, "FASTA_clustering_haploid.fasta")
   greedy_baps <- greedyMix(baps_file, "BAPS")
+  greedy_fasta <- greedyMix(fasta_file, "FASTA")
   expect_type(greedy_baps, "list")
   expect_length(greedy_baps, 10L)
+  expect_type(greedy_fasta, "list")
+  expect_length(greedy_fasta, 10L)
 })
 
 context("Linkage")
